ab87db22b52294d1392929bb9518c561882c6b9b
wujasmine <jasmine.wu02@gmail.com>
2019-09-21 02:15:00 -0500

    Added Homescreen

---
+++ PODER/app/src/main/java/com/codeforgood/poder/HomePage.java
@@ -0,0 +1,28 @@
+package com.codeforgood.poder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.codeforgood.poder.ui.login.LoginActivity;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        final Button test = (Button)findViewById(R.id.getstarted);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
