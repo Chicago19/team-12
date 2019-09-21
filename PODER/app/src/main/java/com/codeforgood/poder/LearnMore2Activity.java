de41943da98f62184c1d6f07cefae1991d287627
Sarah Menza <semenza2@illinois.edu>
2019-09-21 04:02:24 -0500

    adding learn more activities

--- PODER/app/src/main/java/com/codeforgood/poder/LearnMore2Activity.java
+++ PODER/app/src/main/java/com/codeforgood/poder/LearnMore2Activity.java
@@ -2,7 +2,11 @@ package com.codeforgood.poder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LearnMore2Activity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class LearnMore2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_more2);
+        Button next = (Button) findViewById(R.id.next2);
+
+        next.setVisibility(View.VISIBLE);
+        next.setBackgroundColor(Color.TRANSPARENT);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LearnMore2Activity.this, LearnMore3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- PODER/app/src/main/java/com/codeforgood/poder/LearnMore3Activity.java
+++ PODER/app/src/main/java/com/codeforgood/poder/LearnMore3Activity.java
@@ -8,13 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LearnMore1Activity extends AppCompatActivity {
+public class LearnMore3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learn_more1);
-        Button next = (Button) findViewById(R.id.next);
+        setContentView(R.layout.activity_learn_more3);
+        Button next = (Button) findViewById(R.id.next2);
 
         next.setVisibility(View.VISIBLE);
         next.setBackgroundColor(Color.TRANSPARENT);
@@ -22,7 +22,7 @@ public class LearnMore1Activity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LearnMore1Activity.this, LearnMore2Activity.class);
+                Intent intent = new Intent(LearnMore3Activity.this, ExtraElements.class);
                 startActivity(intent);
             }
         });
