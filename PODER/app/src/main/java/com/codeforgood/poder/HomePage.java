5eaa1dc502eba2a0998dfbbee38201017a187b81
wujasmine <jasmine.wu02@gmail.com>
2019-09-21 03:37:06 -0500

    button works

--- PODER/app/src/main/java/com/codeforgood/poder/HomePage.java
+++ PODER/app/src/main/java/com/codeforgood/poder/HomePage.java
@@ -3,6 +3,7 @@ package com.codeforgood.poder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,9 +18,12 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         final Button test = (Button)findViewById(R.id.getstarted);
+        test.setVisibility(View.VISIBLE);
+        test.setBackgroundColor(Color.TRANSPARENT);
 
         test.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
                 Intent intent = new Intent(HomePage.this, LoginActivity.class);
                 startActivity(intent);
             }
