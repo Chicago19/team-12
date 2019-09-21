40ff0a08e1722732470f8a64a5c8574294e5ac2b
Sarah Menza <semenza2@illinois.edu>
2019-09-21 03:35:24 -0500

    adding learn more activities

--- PODER/app/src/main/java/com/codeforgood/poder/LearnMore1Activity.java
+++ PODER/app/src/main/java/com/codeforgood/poder/LearnMore1Activity.java
@@ -2,7 +2,10 @@ package com.codeforgood.poder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LearnMore1Activity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LearnMore1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_more1);
+        Button next = (Button) findViewById(R.id.button3);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LearnMore1Activity.this, LearnMore2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ PODER/app/src/main/java/com/codeforgood/poder/LearnMore2Activity.java
@@ -0,0 +1,14 @@
+package com.codeforgood.poder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnMore2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_more2);
+    }
+}
