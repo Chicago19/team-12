cf190cb9f44d2f2454d21a0f5485b7889e14d44b
Sarah Menza <semenza2@illinois.edu>
2019-09-21 10:34:54 -0500

    adding orientation date

--- PODER/app/src/main/java/com/codeforgood/poder/AppointmentActivity.java
+++ PODER/app/src/main/java/com/codeforgood/poder/AppointmentActivity.java
@@ -2,9 +2,13 @@ package com.codeforgood.poder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class AppointmentActivity extends AppCompatActivity {
 
@@ -15,5 +19,17 @@ public class AppointmentActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_appointment);
+
+        Button date = (Button) findViewById(R.id.date);
+        date.setVisibility(View.VISIBLE);
+        date.setBackgroundColor(Color.TRANSPARENT);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AppointmentActivity.this, OrientationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
