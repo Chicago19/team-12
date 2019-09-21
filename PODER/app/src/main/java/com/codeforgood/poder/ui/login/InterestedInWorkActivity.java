848752a010f733c098ae2525363dddf02e0c9047
Mukhil Murugasamy <mmukhil@gmail.com>
2019-09-21 05:13:24 -0500

    hid title bar on all activities

--- PODER/app/src/main/java/com/codeforgood/poder/ui/login/InterestedInWorkActivity.java
+++ PODER/app/src/main/java/com/codeforgood/poder/ui/login/InterestedInWorkActivity.java
@@ -1,6 +1,8 @@
 package com.codeforgood.poder.ui.login;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,9 @@ class InterestedInWorkActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_interested_in_work);
     }
 }
