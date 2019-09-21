965696687d6b1ee4869ba14f71acd3320d433d7c
Mukhil Murugasamy <mmukhil@gmail.com>
2019-09-21 09:56:24 -0500

    Added submit button for work application

--- PODER/app/src/main/java/com/codeforgood/poder/InterestedInWorkActivity.java
+++ PODER/app/src/main/java/com/codeforgood/poder/InterestedInWorkActivity.java
@@ -1,8 +1,12 @@
 package com.codeforgood.poder;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,5 +21,15 @@ public class InterestedInWorkActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_interested_in_work);
+
+        Button submitb = (Button) findViewById(R.id.button);
+
+        submitb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InterestedInWorkActivity.this, ExtraElements.class);
+                startActivity(intent);
+            }
+        });
     }
 }
